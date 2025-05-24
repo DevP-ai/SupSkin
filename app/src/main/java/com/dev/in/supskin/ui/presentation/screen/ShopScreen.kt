90d6ece61a99d9e09793e1a03f1ce904c590f243
Devajit <patar1447@gmail.com>
2025-05-25 01:35:12 +0530

    Banner with auto scroll implemented

--- app/src/main/java/com/dev/in/supskin/ui/presentation/screen/ShopScreen.kt
+++ app/src/main/java/com/dev/in/supskin/ui/presentation/screen/ShopScreen.kt
@@ -9,6 +9,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dev.`in`.supskin.R
+import com.dev.`in`.supskin.ui.domain.model.BannerData
+import com.dev.`in`.supskin.ui.presentation.screen.components.BannerCarousel
 import com.dev.`in`.supskin.ui.presentation.screen.components.Toolbar
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -22,6 +25,16 @@ fun ShopScreen(modifier: Modifier = Modifier) {
     ) {
         stickyHeader {
             Toolbar()
+        }
+
+        item{
+            val banners = listOf(
+                BannerData(title = "GET 20% OFF", subtitle = "On all electronics", chipText = "12-16 October", image = null),
+                BannerData(title = "FREE DELIVERY", subtitle = "For orders over ₹1000", chipText = "This Week", image = R.drawable.product_img),
+                BannerData(title = "BUY 1 GET 1", subtitle = "On select items", chipText = "Limited Time", image = R.drawable.category_img)
+            )
+
+            BannerCarousel(banners = banners)
         }
     }
 }
